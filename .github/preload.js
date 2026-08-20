e1357d557d43b406e214370c621f296fdba2f3c6
ladunn86 <ladunn666@googlemail.com>
2026-08-20 10:20:17 +0200

    Delete .github/preload.js

--- .github/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-
-  saveJson: (data) => {
-    return ipcRenderer.invoke('save-json', data);
-  }
-
-});
